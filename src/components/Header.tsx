8db6264bac9f63dec5b6c02da6f9f4dc14778615
Антон Щерба <Tony.Shcherba@gmail.com>
2022-11-09 15:15:44 +0300

    feat: make header sticky & animated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,17 @@ import { useAppDispatch, useAppSelector } from 'hooks/hooks';
 import React from 'react';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { authSelector, logOut } from 'store/authSlice';
-import { AppBar, Box, Toolbar, Button, Menu, MenuItem, useMediaQuery } from '@mui/material';
 import { Home, Key, Language, ExitToApp, Dashboard, PersonAdd } from '@mui/icons-material';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Button,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+  useScrollTrigger,
+} from '@mui/material';
 
 export default function Header() {
   const { isAuth } = useAppSelector(authSelector);
@@ -12,6 +21,7 @@ export default function Header() {
   const navigate = useNavigate();
   const [langMenuAnchor, setLangMenuAnchor] = React.useState<null | HTMLElement>(null);
   const isLargeScreen = useMediaQuery('(min-width:420px)');
+  const trigger = useScrollTrigger();
 
   const logOutUser = () => {
     dispatch(logOut());
@@ -27,78 +37,76 @@ export default function Header() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
+    <AppBar color={trigger ? 'default' : 'primary'} position="sticky">
+      <Toolbar>
+        <Button
+          color="inherit"
+          startIcon={isLargeScreen ? <Home /> : null}
+          component={RouterLink}
+          to={Path.home}
+        >
+          Home
+        </Button>
+        <Box sx={{ flexGrow: 1 }} />
+
+        <Box sx={{ display: { md: 'flex', gap: '20px' } }}>
           <Button
             color="inherit"
-            startIcon={isLargeScreen ? <Home /> : null}
-            component={RouterLink}
-            to={Path.home}
+            startIcon={isLargeScreen ? <Language /> : null}
+            onClick={langMenuBtnClick}
           >
-            Home
+            Lang
           </Button>
-          <Box sx={{ flexGrow: 1 }} />
-
-          <Box sx={{ display: { md: 'flex', gap: '20px' } }}>
-            <Button
-              color="inherit"
-              startIcon={isLargeScreen ? <Language /> : null}
-              onClick={langMenuBtnClick}
-            >
-              Lang
-            </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={langMenuAnchor}
-              keepMounted
-              open={Boolean(langMenuAnchor)}
-              onClose={closeLangMenu}
-            >
-              <MenuItem onClick={closeLangMenu}>England</MenuItem>
-              <MenuItem onClick={closeLangMenu}>Russian</MenuItem>
-            </Menu>
-            {isAuth ? (
-              <>
-                <Button
-                  color="inherit"
-                  startIcon={isLargeScreen ? <Dashboard /> : null}
-                  component={RouterLink}
-                  to={Path.boards}
-                >
-                  Boards
-                </Button>
-                <Button
-                  color="inherit"
-                  startIcon={isLargeScreen ? <ExitToApp /> : null}
-                  onClick={logOutUser}
-                >
-                  Log out
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button
-                  color="inherit"
-                  startIcon={isLargeScreen ? <Key /> : null}
-                  component={RouterLink}
-                  to={Path.signIn}
-                >
-                  Sing In
-                </Button>
-                <Button
-                  color="inherit"
-                  startIcon={isLargeScreen ? <PersonAdd /> : null}
-                  component={RouterLink}
-                  to={Path.signUp}
-                >
-                  Sign Up
-                </Button>
-              </>
-            )}
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+          <Menu
+            id="simple-menu"
+            anchorEl={langMenuAnchor}
+            keepMounted
+            open={Boolean(langMenuAnchor)}
+            onClose={closeLangMenu}
+          >
+            <MenuItem onClick={closeLangMenu}>England</MenuItem>
+            <MenuItem onClick={closeLangMenu}>Russian</MenuItem>
+          </Menu>
+          {isAuth ? (
+            <>
+              <Button
+                color="inherit"
+                startIcon={isLargeScreen ? <Dashboard /> : null}
+                component={RouterLink}
+                to={Path.boards}
+              >
+                Boards
+              </Button>
+              <Button
+                color="inherit"
+                startIcon={isLargeScreen ? <ExitToApp /> : null}
+                onClick={logOutUser}
+              >
+                Log out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                color="inherit"
+                startIcon={isLargeScreen ? <Key /> : null}
+                component={RouterLink}
+                to={Path.signIn}
+              >
+                Sing In
+              </Button>
+              <Button
+                color="inherit"
+                startIcon={isLargeScreen ? <PersonAdd /> : null}
+                component={RouterLink}
+                to={Path.signUp}
+              >
+                Sign Up
+              </Button>
+            </>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
