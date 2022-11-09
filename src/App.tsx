d07855f0de3ec1feb1685a604fc165100f7f5a83
Alexander Suslov <sasha_suslov07@mail.ru>
2022-11-09 05:05:18 +0300

    feat: add home page primary layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from 'components/Layout';
+import Home from 'pages/Home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
