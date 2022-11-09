19d33e8faacdc3c02ef780ce4728a0b7851a7409
Антон Щерба <Tony.Shcherba@gmail.com>
2022-11-09 13:44:34 +0300

    feat: implement responsive header layout with MUI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,104 @@
-import { styled } from '@mui/material';
 import { Path } from 'constants/routing';
 import { useAppDispatch, useAppSelector } from 'hooks/hooks';
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { authSelector, logOut } from 'store/authSlice';
+import { AppBar, Box, Toolbar, Button, Menu, MenuItem, useMediaQuery } from '@mui/material';
+import { Home, Key, Language, ExitToApp, Dashboard, PersonAdd } from '@mui/icons-material';
 
-const StyledHeader = styled('header')({
-  display: 'flex',
-  justifyContent: 'space-evenly',
-  alignItems: 'center',
-});
-
-const Header = () => {
+export default function Header() {
   const { isAuth } = useAppSelector(authSelector);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const [langMenuAnchor, setLangMenuAnchor] = React.useState<null | HTMLElement>(null);
+  const isLargeScreen = useMediaQuery('(min-width:420px)');
 
   const logOutUser = () => {
     dispatch(logOut());
     navigate(Path.home);
   };
 
-  return (
-    <StyledHeader>
-      <Link to={Path.home}>Home</Link>
-      <p>Lang</p>
-      {isAuth ? (
-        <>
-          <Link to={Path.boards}>Boards</Link>
-          <button onClick={logOutUser}>Log out </button>
-        </>
-      ) : (
-        <>
-          <Link to={Path.signIn}>Sing In</Link>
-          <Link to={Path.signUp}>Sign Up</Link>
-        </>
-      )}
-    </StyledHeader>
-  );
-};
+  const langMenuBtnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setLangMenuAnchor(event.currentTarget);
+  };
 
-export default Header;
+  const closeLangMenu = () => {
+    setLangMenuAnchor(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button
+            color="inherit"
+            startIcon={isLargeScreen ? <Home /> : null}
+            component={RouterLink}
+            to={Path.home}
+          >
+            Home
+          </Button>
+          <Box sx={{ flexGrow: 1 }} />
+
+          <Box sx={{ display: { md: 'flex', gap: '20px' } }}>
+            <Button
+              color="inherit"
+              startIcon={isLargeScreen ? <Language /> : null}
+              onClick={langMenuBtnClick}
+            >
+              Lang
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={langMenuAnchor}
+              keepMounted
+              open={Boolean(langMenuAnchor)}
+              onClose={closeLangMenu}
+            >
+              <MenuItem onClick={closeLangMenu}>England</MenuItem>
+              <MenuItem onClick={closeLangMenu}>Russian</MenuItem>
+            </Menu>
+            {isAuth ? (
+              <>
+                <Button
+                  color="inherit"
+                  startIcon={isLargeScreen ? <Dashboard /> : null}
+                  component={RouterLink}
+                  to={Path.boards}
+                >
+                  Boards
+                </Button>
+                <Button
+                  color="inherit"
+                  startIcon={isLargeScreen ? <ExitToApp /> : null}
+                  onClick={logOutUser}
+                >
+                  Log out
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  color="inherit"
+                  startIcon={isLargeScreen ? <Key /> : null}
+                  component={RouterLink}
+                  to={Path.signIn}
+                >
+                  Sing In
+                </Button>
+                <Button
+                  color="inherit"
+                  startIcon={isLargeScreen ? <PersonAdd /> : null}
+                  component={RouterLink}
+                  to={Path.signUp}
+                >
+                  Sign Up
+                </Button>
+              </>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
