48a9e0b7d136e840f925409e2d1b89c5dbc8e54a
Антон Щерба <Tony.Shcherba@gmail.com>
2022-11-11 03:18:25 +0300

    feat: implement Boards page draft layout

--- src/pages/Boards.tsx
+++ src/pages/Boards.tsx
@@ -1,12 +1,28 @@
 import Page from 'components/Page';
 import React from 'react';
+import { Button, Box, Typography, useMediaQuery } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import BoardPreview from 'components/BoardPreview';
 
-const Boards = () => {
+export default function Boards() {
+  const isLargeScreen = useMediaQuery('(min-width:380px)');
+  const { t } = useTranslation('translation', { keyPrefix: 'boardList' });
+  const style = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    gap: 4,
+    mx: isLargeScreen ? 4 : 1,
+  };
+
   return (
     <Page>
-      <h1>Here will be board list</h1>
+      <Box sx={style}>
+        <BoardPreview name="name" description="caption text" />
+        <Button sx={{ width: 310, height: 310 }} variant="outlined">
+          <Typography variant="h4">{t('add')}</Typography>
+        </Button>
+      </Box>
     </Page>
   );
-};
-
-export default Boards;
+}
