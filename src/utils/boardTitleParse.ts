61adf84812472ecb157ed1f4eed84d68b37198a2
Антон Щерба <Tony.Shcherba@gmail.com>
2022-11-14 23:46:58 +0300

    feat: add boardTitleDataParse functions

--- src/utils/boardTitleParse.ts
+++
@@ -1,8 +0,0 @@
-interface IBoardTitleParse {
-  title: string;
-  description: string;
-}
-
-export default function boardTitleParse(title: string) {
-  return JSON.parse(title) as IBoardTitleParse;
-}
