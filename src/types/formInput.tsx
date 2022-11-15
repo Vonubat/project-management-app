25a280224a9ed1cb14f9b2c0738ff2b4ce685b1f
Антон Щерба <Tony.Shcherba@gmail.com>
2022-11-15 19:41:56 +0300

    feat: implement EditBoardForm component

--- src/types/formInput.tsx
+++ src/types/formInput.tsx
@@ -1,17 +1,19 @@
 import { Control, RegisterOptions } from 'react-hook-form';
 import { SignInFormFields, SignUpFormFields } from './auth';
+import { EditBoardFormFields } from 'types/boards';
 
 // Add here your form fields types as intersection
-export type CustomFormFields = SignInFormFields & SignUpFormFields;
+export type CustomFormFields = SignInFormFields & SignUpFormFields & EditBoardFormFields;
 
 // Add here your form fields keys
-export type InputName = keyof SignUpFormFields;
+export type InputName = keyof CustomFormFields;
 
 export type InputOptions = {
   name: InputName;
   label: string;
   type: 'password' | 'text';
   validationOptions: RegisterOptions;
+  rows?: number;
 };
 
 export type FormControl = Control<Partial<CustomFormFields>, unknown>;
