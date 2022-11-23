7f40645264d2147c87a35c2a3b55c9eac7c7b391
Антон Щерба <Tony.Shcherba@gmail.com>
2022-11-23 15:10:21 +0300

    feat: remove the add button from the Boards page

--- src/pages/BoardList.tsx
+++ src/pages/BoardList.tsx
@@ -1,16 +1,13 @@
 import React, { useEffect, forwardRef } from 'react';
-import { Button, Box, Typography, useMediaQuery, CircularProgress } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import styled from '@emotion/styled';
-import { useTranslation } from 'react-i18next';
 import Page from 'components/Page';
 import BoardPreview from 'components/BoardPreview';
 import EditBoardForm from 'components/forms/EditBoardForm';
 import { boardListSelector, getBoardsByUser } from 'store/boardListSlice';
-import { clearBoardParams, openModalForm } from 'store/modalSlice';
 import { useAppDispatch, useAppSelector } from 'hooks/hooks';
-import { MediaQuery, TypeofModal } from 'constants/constants';
+import { MediaQuery } from 'constants/constants';
 import { getAllUsers } from 'store/usersSlice';
-
 import FlipMove from 'react-flip-move';
 import { BoardData } from 'types/boards';
 
@@ -18,7 +15,7 @@ const StyledBox = styled(FlipMove)({
   display: 'flex',
   flexWrap: 'wrap',
   justifyContent: 'center',
-  gap: 4,
+  gap: 10,
 });
 
 const FunctionalArticle = forwardRef<HTMLDivElement, BoardData>((props, ref) => (
@@ -29,19 +26,13 @@ const FunctionalArticle = forwardRef<HTMLDivElement, BoardData>((props, ref) => 
 
 export default function Boards() {
   const isLargeScreen = useMediaQuery(MediaQuery.minWidth380);
-  const { t } = useTranslation('translation', { keyPrefix: 'boardList' });
-  const { boards, isAddBoardLoading } = useAppSelector(boardListSelector);
+  const { boards } = useAppSelector(boardListSelector);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getBoardsByUser());
     dispatch(getAllUsers());
   }, [dispatch]);
-
-  function openAddBoardModalForm() {
-    dispatch(clearBoardParams());
-    dispatch(openModalForm(TypeofModal.board));
-  }
 
   return (
     <Page>
@@ -50,18 +41,6 @@ export default function Boards() {
           {boards.map((board) => (
             <FunctionalArticle key={board._id} {...board} />
           ))}
-          <Button
-            sx={{ width: 310, height: 310 }}
-            variant="outlined"
-            onClick={openAddBoardModalForm}
-            disabled={isAddBoardLoading}
-          >
-            {isAddBoardLoading ? (
-              <CircularProgress color="inherit" size={100} />
-            ) : (
-              <Typography variant="h4">{t('add')}</Typography>
-            )}
-          </Button>
         </StyledBox>
       </Box>
       <EditBoardForm />
