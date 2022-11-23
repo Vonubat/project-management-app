ca3a4b4d25319870157a29cb0d0d3dd535d4989f
Антон Щерба <Tony.Shcherba@gmail.com>
2022-11-23 15:25:09 +0300

    refactor: make a separate component SearchBar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import { InputBase } from '@mui/material';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  width: '100%',
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 2, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+type Props = {
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+};
+
+const SearchBar: React.FC<Props> = ({ value, onChange }) => {
+  const { t } = useTranslation('translation', { keyPrefix: 'boardList' });
+
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder={t('search')}
+        inputProps={{ 'aria-label': `${t('search')}` }}
+        value={value}
+        onChange={onChange}
+      />
+    </Search>
+  );
+};
+
+export default SearchBar;
--- src/components/UserSearchBar.tsx
+++ src/components/UserSearchBar.tsx
@@ -1,10 +1,7 @@
 import React, { FC, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import { SignUpOkResponseData as UserData } from 'types/auth';
-import { styled, alpha } from '@mui/material/styles';
-import SearchIcon from '@mui/icons-material/Search';
+import SearchBar from 'components/SearchBar';
 import {
-  InputBase,
   List,
   ListItem,
   ListItemButton,
@@ -14,40 +11,6 @@ import {
   Paper,
   ListItemIcon,
 } from '@mui/material';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  width: '100%',
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 2, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
 
 const checkListStyles = {
   height: 153,
@@ -65,7 +28,6 @@ type Props = {
 };
 
 const UserSearchBar: FC<Props> = ({ userId, users, checkedUsersID, handleToggle }) => {
-  const { t } = useTranslation('translation', { keyPrefix: 'boardList' });
   const [searchValue, setSearchValue] = useState('');
   const filteredUsers = users.filter(({ name, login, _id }) => {
     const lowName = name.toLowerCase();
@@ -76,17 +38,7 @@ const UserSearchBar: FC<Props> = ({ userId, users, checkedUsersID, handleToggle 
 
   return (
     <Paper variant="outlined" sx={{ ':hover': { borderColor: 'black' }, mt: 2, mb: 1 }}>
-      <Search>
-        <SearchIconWrapper>
-          <SearchIcon />
-        </SearchIconWrapper>
-        <StyledInputBase
-          placeholder={t('search')}
-          inputProps={{ 'aria-label': `${t('search')}` }}
-          value={searchValue}
-          onChange={(event) => setSearchValue(event.target.value)}
-        />
-      </Search>
+      <SearchBar value={searchValue} onChange={(event) => setSearchValue(event.target.value)} />
       <Divider />
       <List className="alternative-scroll" sx={checkListStyles}>
         {filteredUsers.map(({ name, login, _id }) => (
