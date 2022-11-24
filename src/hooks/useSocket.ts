8b3b68cb2ef87a216555620ea7c9bc1e1d169ab9
Alexander Suslov <sasha_suslov07@mail.ru>
2022-11-24 13:49:29 +0300

    feat: create websocket functionality for board page

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,20 @@
+import { BASE_URL } from 'constants/constants';
+import { useEffect, useRef } from 'react';
+import io, { ManagerOptions, Socket, SocketOptions } from 'socket.io-client';
+
+export const useSocket = (
+  url: string = BASE_URL,
+  options: Partial<ManagerOptions & SocketOptions> = { autoConnect: false }
+): Socket => {
+  const { current: socket } = useRef(io(url, options));
+
+  useEffect(() => {
+    return () => {
+      if (socket) {
+        socket.close();
+      }
+    };
+  }, [socket]);
+
+  return socket;
+};
