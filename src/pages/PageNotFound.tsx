6cc912d79b5293ae54bd4bef7b8bca850f96fac2
Tabunov Egor <Vonubat@gmail.com>
2022-12-02 16:55:36 +0300

    fix: PageNotFound styles

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -2,11 +2,20 @@ import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link as RouterLink } from 'react-router-dom';
 import ErrorIcon from '@mui/icons-material/Error';
-import { Button, Typography } from '@mui/material';
+import { Box, Button, styled, Typography } from '@mui/material';
 import { DefaultColors } from 'constants/constants';
 import { Path } from 'constants/routing';
 
 import Page from 'components/Page';
+
+const StyledBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  textAlign: 'center',
+  padding: '3rem',
+  gap: '1rem',
+});
 
 const PageNotFound: FC = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'pageNotFound' });
@@ -14,29 +23,31 @@ const PageNotFound: FC = () => {
   return (
     <Page
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyItems: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        marginTop: '1rem',
-        marginBottom: '3rem',
-        px: 2,
-        gap: 2,
+        p: 0,
       }}
     >
-      <ErrorIcon color={DefaultColors.error} sx={{ fontSize: 300 }} />
-      <Typography variant="h1">404</Typography>
-      <Typography variant="h3">{t('title')}</Typography>
-      <Typography variant="h5">{t('description')}</Typography>
-      <Button
-        variant="contained"
-        sx={{ width: 'fit-content', whiteSpace: 'nowrap' }}
-        component={RouterLink}
-        to={Path.home}
-      >
-        {t('btn')}
-      </Button>
+      <StyledBox>
+        <ErrorIcon color={DefaultColors.error} sx={{ fontSize: 250 }} />
+        <StyledBox
+          sx={{
+            borderRadius: 5,
+            backgroundColor: 'rgba(255,255,255, 0.7)',
+          }}
+        >
+          <Typography variant="h1">404</Typography>
+          <Typography variant="h3">{t('title')}</Typography>
+          <Typography variant="h5">{t('description')}</Typography>
+        </StyledBox>
+
+        <Button
+          variant="contained"
+          sx={{ width: 'fit-content', whiteSpace: 'nowrap' }}
+          component={RouterLink}
+          to={Path.home}
+        >
+          {t('btn')}
+        </Button>
+      </StyledBox>
     </Page>
   );
 };
